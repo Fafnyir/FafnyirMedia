0ea0b9e466893b982975ee7f30ff4a18d8da61db
Fafnyir <frank@frankpatten.ca>
2026-01-28 19:15:27 -0500

    Add files via upload

---
+++ FafnyirMedia/FafnyirMedia.lua
@@ -0,0 +1,40 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+
+-- ----- 
+--  MEDIA
+--  Interface\Addons\FafnyirMedia\logo\AddonLogo.tga
+-- ---- 
+
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "- The Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Regular.ttf]])
+LSM:Register("font", "- The Bold Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Bold.ttf]])
+LSM:Register("font", "- The Medium Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Medium.ttf]])
+LSM:Register("font", "- DAMAGE", [[Interface\Addons\FafnyirMedia\fonts\LuckiestGuy.ttf]])
+
+
+
+
+
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+-- LSM:Register ("statusbar", "- Half", [[Interface\Addons\FafnyirMedia\statusbar\Half.tga]])
+LSM:Register ("statusbar", "- Flat", [[Interface\Addons\FafnyirMedia\statusbar\Flat.tga]])
+LSM:Register ("statusbar", "- Blank", [[Interface\Addons\FafnyirMedia\statusbar\Blank.tga]])
+LSM:Register ("statusbar", "- Clean", [[Interface\Addons\FafnyirMedia\statusbar\Clean.tga]])
+LSM:Register ("statusbar", "- CastBar", [[Interface\Addons\FafnyirMedia\statusbar\CastBar.tga]])
+LSM:Register ("statusbar", "- Glass", [[Interface\Addons\FafnyirMedia\statusbar\Wglass.tga]])
+LSM:Register ("statusbar", "- Blizz", [[Interface\Addons\FafnyirMedia\statusbar\BlizzardCast.tga]])
+LSM:Register ("statusbar", "- Midnight Background", [[Interface\Addons\FafnyirMedia\statusbar\midnight_background.blp]])
+LSM:Register ("statusbar", "- Midnight Foreground", [[Interface\Addons\FafnyirMedia\statusbar\midnight_foreground.blp]])
