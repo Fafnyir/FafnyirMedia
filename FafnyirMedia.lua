3e998b89a190e33958529caae1afbbbe50e0c2c2
Fafnyir <frank@frankpatten.ca>
2026-02-22 21:44:41 -0500

    Bar Addition

--- FafnyirMedia.lua
+++ FafnyirMedia.lua
@@ -32,3 +32,4 @@ LSM:Register("font", "- THE FONT", [[Interface\Addons\FafnyirMedia\fonts\Afacad-
 LSM:Register ("statusbar", "- Clean", [[Interface\Addons\FafnyirMedia\statusbar\Clean.tga]])
 LSM:Register ("statusbar", "- CastBar", [[Interface\Addons\FafnyirMedia\statusbar\CastBar.tga]])
 LSM:Register ("statusbar", "- Blizz", [[Interface\Addons\FafnyirMedia\statusbar\BlizzardCast.tga]])
+LSM:Register ("statusbar", "- FafnyirBar", [[Interface\Addons\FafnyirMedia\statusbar\FafnyirBar.tga]])
