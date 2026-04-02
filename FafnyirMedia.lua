bc1f5792a644634c247fd8cdbd75f63c77518cc2
Fafnyir <frank@frankpatten.ca>
2026-04-02 17:16:13 -0400

    Update FafnyirMedia.lua

--- FafnyirMedia.lua
+++ FafnyirMedia.lua
@@ -36,4 +36,4 @@ LSM:Register ("statusbar", "- Clean", [[Interface\Addons\FafnyirMedia\statusbar\
 LSM:Register ("statusbar", "- Flat", [[Interface\Addons\FafnyirMedia\statusbar\Flat.tga]])
 LSM:Register ("statusbar", "- CastBar", [[Interface\Addons\FafnyirMedia\statusbar\CastBar.tga]])
 LSM:Register ("statusbar", "- Blizz", [[Interface\Addons\FafnyirMedia\statusbar\BlizzardCast.tga]])
-LSM:Register ("statusbar", "- FafnyirBar", [[Interface\Addons\FafnyirMedia\statusbar\Meleii.tga]])
+LSM:Register ("statusbar", "- Meleii", [[Interface\Addons\FafnyirMedia\statusbar\Meleii.tga]])
